e3daf22859a5eba4e6d733c24dbadef7485a01bd
mdukes0 <mdukes@una.edu>
2025-10-01 10:43:18 -0500

    Add files via upload

---
+++ scripts/cardGAmeScripts.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("document successfully loaded")
+
+	const randomDeckOfCards = makeDeckOfCards();
+	console.log("random Deck: ", makeDeckOfCards());
+
+	//TODO: make them random
+
+	console.log(Math.floor(Math.random() * 52));
+
+	randomDeckOfCards.forEach((card) => {
+	let cardP = document.createElement('P');
+	cardP.ClassName("text-wrap");
+	cardP.innerText(card);
+	console.log(cardP);
+	document.getElementById('stackOfCards').append(card[4])
+}
+);
+
+
+}
+);
+
+function makeDeckOfCards() {
+
+	//make 52 cards, 4 suits, 13 per
+
+	//var = ... let or const
+
+	// hard code = const deck = [[kheart, qheart, jheart], [kspades...]]
+
+	const suits = ["♥️", "♠️", "♣️", "♦️"]
+	let deckOfCards = []; //initialize empty array
+
+	for (i = 0; i <= 13; i++) {
+		suits.forEach((suit) => {
+			//console.log(suit, i);
+			deckOfCards.push(suit + i);
+		});
+	}
+
+	//If it's correct make green, if wrong then red
+	//Condition where if all droppable are green it will pop up win screen
+	//create a score board
+
+
+	//TODO: user name
+	$('btnSaveUserName').on('click', ()=> {
+		let $userName = $('#userName').val();
+		console.log("user name set by code: ", $userName);
+
+		$('body').append('<br><p>welcome' + $userName + '</p>')
+	})
+	document.getElementsByTagName
+
+	//Set a cookie
+	
+	//TODO: score
+	//TODO: rounds
+}
+
